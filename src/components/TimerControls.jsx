46a0b698008727df692f6d8703ee56aaf70ec78e
Ninad Shirsat <ninad@192.168.1.3>
2025-11-14 10:27:37 +0530

    fixed css

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -14,14 +14,14 @@ const TimerControls = ({ onToggle, onReset, isRunning }) => {
       <button
         ref={starButtontRef}
         onClick={onToggle}
-        className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover: bg-green-600 mr-3"
+        className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 mr-3"
       >
         {isRunning ? "Pause" : "Start"}
       </button>
 
       <button
         onClick={onReset}
-        className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover bg-red-600"
+        className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-800"
       >
         Reset
       </button>
